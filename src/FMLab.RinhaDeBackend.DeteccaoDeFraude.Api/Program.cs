30e318d0880cb71939c0dddc0dfff8041feba9fb
Fagnerm <fagnermarinho.dev@gmail.com>
2026-05-25 22:47:30 -0300

    Substituindo Unix socket por TCP na comunicacao proxy/APIs

--- src/FMLab.RinhaDeBackend.DeteccaoDeFraude.Api/Program.cs
+++ src/FMLab.RinhaDeBackend.DeteccaoDeFraude.Api/Program.cs
@@ -2,7 +2,6 @@ using FMLab.RinhaDeBackend.DeteccaoDeFraude.Api.Infrastructure.ReferenceData;
 using FMLab.RinhaDeBackend.DeteccaoDeFraude.Api.Infrastructure.Serialization;
 using FMLab.RinhaDeBackend.DeteccaoDeFraude.Api.Infrastructure.VectorStore;
 using FMLab.RinhaDeBackend.DeteccaoDeFraude.Features.FraudDetection;
-using Microsoft.AspNetCore.Server.Kestrel.Transport.Sockets;
 
 // Run offline index build: dotnet run -- --build-index
 if (args.Contains("--build-index"))
@@ -16,13 +15,6 @@ if (args.Contains("--build-index"))
 var builder = WebApplication.CreateSlimBuilder(args);
 builder.Logging.ClearProviders();
 
-var socketPath = Environment.GetEnvironmentVariable("SOCKET_PATH");
-if (!string.IsNullOrEmpty(socketPath))
-{
-    if (File.Exists(socketPath)) File.Delete(socketPath);
-    builder.WebHost.ConfigureKestrel(k => k.ListenUnixSocket(socketPath));
-}
-
 builder.Services.ConfigureHttpJsonOptions(o =>
     o.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonContext.Default));
 
@@ -34,15 +26,6 @@ var handler       = new FraudDetectionHandler(referenceData, vectorStore, respon
 await vectorStore.LoadAsync();
 
 var app = builder.Build();
-
-if (!string.IsNullOrEmpty(socketPath))
-{
-    app.Lifetime.ApplicationStarted.Register(() =>
-        File.SetUnixFileMode(socketPath,
-            UnixFileMode.UserRead  | UnixFileMode.UserWrite  |
-            UnixFileMode.GroupRead | UnixFileMode.GroupWrite |
-            UnixFileMode.OtherRead | UnixFileMode.OtherWrite));
-}
 
 app.Use(async (ctx, next) =>
 {
